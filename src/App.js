730be4e4813831752c2bfcac4941dfd5bde761c4
Andre Ostan <andre.ostan@gmail.com>
2023-11-05 10:11:41 -0300

    End of Section 6.33

--- src/App.js
+++ src/App.js
@@ -1,58 +1,43 @@
-import { v4 as uuidv4 } from "uuid";
 import {
   BrowserRouter as Router,
   Route,
   Routes,
-  NavLink,
+  // NavLink,
 } from "react-router-dom";
 // import Card from "./components/shared/Card";
-import { useState } from "react";
+// import { useState } from "react";
 import Header from "./components/Header";
 import FeedbackList from "./components/FeedbackList";
 import FeedbackStats from "./components/FeedbackStats";
 import FeedbackForm from "./components/FeedbackForm";
 import AboutIconLink from "./components/AboutIconLink";
 import AboutPage from "./pages/AboutPage";
+import { FeedbackProvider } from "./context/FeedbackContext";
 // import Post from "./components/Post";
-import FeedbackData from "./data/FeedbackData";
+// import FeedbackData from "./data/FeedbackData";
 
 function App() {
-  const [feedback, setFeedback] = useState(FeedbackData);
-
-  const addFeedback = (newFeedback) => {
-    newFeedback.id = uuidv4();
-    setFeedback([newFeedback, ...feedback]);
-  };
-
-  const deleteFeedback = (id) => {
-    if (window.confirm("Are you sure you want do delete?")) {
-      setFeedback(feedback.filter((item) => item.id !== id));
-    }
-  };
-
   return (
-    <Router>
-      <Header />
-      <div className="container">
-        <Routes>
-          <Route
-            exact
-            path="/"
-            element={
-              <>
-                <FeedbackForm handleAdd={addFeedback} />
-                <FeedbackStats feedback={feedback} />
-                <FeedbackList
-                  feedback={feedback}
-                  handleDelete={deleteFeedback}
-                />
-              </>
-            }
-          ></Route>
-          <Route path="/about" element={<AboutPage />} />
-          {/* <Route path="/post/*" element={<Post />} /> */}
-        </Routes>
-        {/* <Card>
+    <FeedbackProvider>
+      <Router>
+        <Header />
+        <div className="container">
+          <Routes>
+            <Route
+              exact
+              path="/"
+              element={
+                <>
+                  <FeedbackForm />
+                  <FeedbackStats />
+                  <FeedbackList />
+                </>
+              }
+            ></Route>
+            <Route path="/about" element={<AboutPage />} />
+            {/* <Route path="/post/*" element={<Post />} /> */}
+          </Routes>
+          {/* <Card>
           <NavLink to="/" activeClassName="active">
             Home
           </NavLink>
@@ -60,9 +45,10 @@ function App() {
             About
           </NavLink>
         </Card> */}
-        <AboutIconLink />
-      </div>
-    </Router>
+          <AboutIconLink />
+        </div>
+      </Router>
+    </FeedbackProvider>
   );
 }
 
---
+++ src/context/FeedbackContext.jsx
@@ -0,0 +1,57 @@
+import { v4 as uuidv4 } from "uuid";
+import { createContext, useState } from "react";
+
+const FeedbackContext = createContext();
+
+export const FeedbackProvider = ({ children }) => {
+  const [feedback, setFeedback] = useState([
+    {
+      id: 1,
+      text: "This item is feedback item 1",
+      rating: 10,
+    },
+    {
+      id: 2,
+      text: "This item is feedback item 2",
+      rating: 8,
+    },
+    {
+      id: 3,
+      text: "This item is feedback item 3",
+      rating: 6,
+    },
+  ]);
+
+  const [feedbackEdit, setFeedbackEdit] = useState({
+    item: {},
+    edit: false,
+  });
+
+  // Delete feedback
+  const deleteFeedback = (id) => {
+    if (window.confirm("Are you sure you want do delete?")) {
+      setFeedback(feedback.filter((item) => item.id !== id));
+    }
+  };
+
+  // Add feedback
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuidv4();
+    setFeedback([newFeedback, ...feedback]);
+  };
+
+  // Set item to be updated
+  const editFeedback = (item) => {
+    setFeedbackEdit({ item, edit: true });
+  };
+
+  return (
+    <FeedbackContext.Provider
+      value={{ feedback, deleteFeedback, addFeedback, editFeedback }}
+    >
+      {children}
+    </FeedbackContext.Provider>
+  );
+};
+
+export default FeedbackContext;
